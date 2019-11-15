f8ba9e8c45d4e00b51d60851f288390f1fdf3ff8
Kevin <kevinrivera.a4b@gmail.com>
2019-11-15 12:29:18 +0100

    Master 1- test on hosting

---
+++ precache-manifest.f9cde3c53772a379e653bea873fe3bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d284366b830650351294a54f6dd29a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9354c433f517c1ce749",
+    "url": "/static/css/main.bc86dbc7.chunk.css"
+  },
+  {
+    "revision": "c12ae7de86038ddc6e11",
+    "url": "/static/js/2.4ae5484c.chunk.js"
+  },
+  {
+    "revision": "f9354c433f517c1ce749",
+    "url": "/static/js/main.f7af16c7.chunk.js"
+  },
+  {
+    "revision": "2fe10b42a886ea67b818",
+    "url": "/static/js/runtime-main.d77f4095.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
